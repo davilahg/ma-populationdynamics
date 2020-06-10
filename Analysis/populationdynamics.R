c4a137d651f1acc761d58168f97ab2976a51f534
gerardo <davilahg@ciencias.unam.mx>
2020-06-10 13:32:11 -0500

    delete one line

--- Analysis/populationdynamics.R
+++ Analysis/populationdynamics.R
@@ -2,7 +2,6 @@
 #
 setwd("/Users/Edgar/Google\ Drive/Edgar/Trabajo/FC/Dirección\ de\ tesis/L\ 2017\ Gerardo\ Dávila/")
 # load requiered libraries
-# probando
 library(AICcmodavg)
 library(betareg)
 library(EnvStats)
