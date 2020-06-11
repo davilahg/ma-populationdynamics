25685fd513e0e2c40804db1711b8ef693f06c7d0
davilahg <58412540+davilahg@users.noreply.github.com>
2020-06-11 00:10:35 -0500

    Update populationdynamics.R

--- Analysis/populationdynamics.R
+++ Analysis/populationdynamics.R
@@ -1,7 +1,7 @@
 # Population dynamics of Mimosa acantholoba
 #
 setwd("/Users/Edgar/Google\ Drive/Edgar/Trabajo/FC/Dirección\ de\ tesis/L\ 2017\ Gerardo\ Dávila/")
-# load requiered libraries: hola
+# load requiered libraries:
 # install.packages(c('AICcmodavg', 'betareg', 'EnvStats', 'gamm4', 'ggplot2', 'mgcv', 'reshape2', 'gridExtra', 'plot3D', 'fields'))
 library(AICcmodavg)
 library(betareg)
